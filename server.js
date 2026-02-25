54f3cfc67a13fb44d0c9637a8b5fc6af49c178fd
SevenSonics <Juanmgutierrezf@gmail.com>
2026-02-25 01:38:47 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Box Configurator Engine Running");
+});
+
+app.post("/calculate", (req, res) => {
+
+  const { L, W, H, qty } = req.body;
+
+  const flatWidth = (2 * L) + (2 * H) + 15;
+  const flatHeight = W + (2 * H);
+
+  const area = (flatWidth/1000) * (flatHeight/1000);
+  const unitCost = area * 6;
+  const total = unitCost * qty * 1.35;
+
+  res.json({
+    flatWidth,
+    flatHeight,
+    total
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running");
+});

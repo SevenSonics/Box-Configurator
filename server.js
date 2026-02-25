2d1bea59d635689dea095c82cc5381ee9a39ccf9
SevenSonics <Juanmgutierrezf@gmail.com>
2026-02-25 01:45:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Box Configurator Engine Running");
